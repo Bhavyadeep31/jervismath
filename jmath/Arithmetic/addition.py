c95882fbf9b7b90ec00f3afb69d63e9c9f27163a
Bhavyadeep31 <bhavyadeep31yadav@gmail.com>
2021-06-24 22:40:14 +0530

    New Function: `Addition` | It can add number no matter how many there are.

--- jmath/Arithmetic/addition.py
+++ jmath/Arithmetic/addition.py
@@ -1,5 +1,25 @@
+'''
+Information:
+            Use this function to add number no matter how many.
+
+Syntax:
+        add(...)
+
+Example:
+        add(1+2+3+4+5)
+        add(142+32532+523+25325)
+
+Note:
+    You need to add this inside the print statement so as to get the result printed in the terminal.
+'''
+
 def add(*numbers):
     result = 0
     for number in numbers:
         result += number
     return result
+
+"""
+Made by:
+        Bhavyadeep31
+"""
