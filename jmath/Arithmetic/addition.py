f7dda6e450743188fbce657babf1f4e77fd8c6b4
Bhavyadeep31 <bhavyadeep31yadav@gmail.com>
2021-06-24 23:02:20 +0530

    Command Edit: `Addition` | Changed the way the calculation was to happen | Edited the information about the command accordingly!

--- jmath/Arithmetic/addition.py
+++ jmath/Arithmetic/addition.py
@@ -3,19 +3,19 @@ Information:
             Use this function to add number no matter how many.
 
 Syntax:
-        add(...)
+        add([...])
 
 Example:
-        add(1+2+3+4+5)
-        add(142+32532+523+25325)
+        add([1, 2, 3, 4, 5])
+        add([142, 32532, 523, 25325])
 
 Note:
     - You need to add this inside the print statement so as to get the result printed in the terminal.
 '''
 
-def add(*numbers):
+def add(numberlist):
     result = 0
-    for number in numbers:
+    for number in numberlist:
         result += number
     return result
 
